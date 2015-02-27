bcb9f873abd8076736316fd2205543c646bf4723
Thomas Jensen <thomas@dragonlancers.com>
2015-02-27 01:54:22 +0100

    added mandrill secret 2nd fix imported mail class

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,6 +2,7 @@
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Mail;
 class EmailController extends Controller {
 
 	/**
