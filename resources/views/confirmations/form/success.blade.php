264ff868a4b4af90d35945ea6eed34f0565a6c7e
Thomas Jensen <thomas@dragonlancers.com>
2015-03-01 16:12:48 +0100

    fixed success page img

--- resources/views/confirmations/form/success.blade.php
+++ resources/views/confirmations/form/success.blade.php
@@ -5,7 +5,7 @@
 
     <section class="confirmation">
         <figure>
-            <a href="https://www.dragonlancers.com"><img src="{{ asset('images/cute-mr-dragon-thanks-page.png') }}" width="100%" alt="Thanks..."> </a>
+            <a href="https://www.dragonlancers.com"><img src="{{ asset('images/cute-mr-dragon-success-page.png') }}" width="100%" alt="Thanks..."> </a>
             <figcaption><span>{{ trans('confirmations.success-contact') }}</span></figcaption>
         </figure>
     </section>
