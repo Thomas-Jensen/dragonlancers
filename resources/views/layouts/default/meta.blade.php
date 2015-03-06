cd3e234d852975a48e1d47d5a33fa4f5f96fcd82
Thomas Jensen <thomas@dragonlancers.com>
2015-03-06 01:26:32 +0100

    new images

--- resources/views/layouts/default/meta.blade.php
+++ resources/views/layouts/default/meta.blade.php
@@ -4,7 +4,7 @@
 <!-- Schema.org markup -->
 <meta itemprop="name" content="{{ $title }}">
 <meta itemprop="description" content="{{ $descriptionSchemaorg }}">
-<meta itemprop="image" content="https://www.dragonlancers.com/images/dllogo.png">
+<meta itemprop="image" content="https://www.dragonlancers.com/images/dl-logo.png">
 
 <!-- Twitter Card data -->
 <meta name="twitter:card" content="summary_large_image">
@@ -13,13 +13,13 @@
 <meta name="twitter:description" content="{{ $descriptionTwitter }}">
 <meta name="twitter:creator" content="@dragonlancersco">
 <!-- Twitter summary card with large image must be at least 280x150px -->
-<meta name="twitter:image:src" content="https://www.dragonlancers.com/images/dllogo.png">
+<meta name="twitter:image:src" content="https://www.dragonlancers.com/images/dl-logo.png">
 
 <!-- Open Graph data -->
 <meta property="og:title" content="{{ $title }}" />
 <meta property="og:type" content="{{ $typeOpengraph }}" />
 <meta property="og:url" content="{{ Request::URL()}}" />
-<meta property="og:image" content="https://www.dragonlancers.com/images/dllogo.png" />
+<meta property="og:image" content="https://www.dragonlancers.com/images/dl-logo.png" />
 <meta property="og:description" content="{{ $descriptionOpengraph }}" />
 <meta property="og:site_name" content="Dragon Lancers" />
 <meta property="fb:admins" content="100001461555837"/>
